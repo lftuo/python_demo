f485a06c081f02048750d3618edf44a0fd052ce0
lftuo <909709223@qq.com>
2018-01-02 17:34:31 +0800

    上传最新代码

--- ten_mintes_to_pandas/Operations.py
+++ ten_mintes_to_pandas/Operations.py
@@ -4,3 +4,17 @@
 # @Time : 2018-1-2 17:00:31
 # @File : Operations.py
 # @Software : IntelliJ IDEA
+import pandas as pd
+import numpy as np
+
+dates = pd.date_range('20130101', periods=6)
+df = pd.DataFrame(np.random.randn(6,4),index=dates,columns=list('ABCD'))
+print '-----------------------------Stats----------------------------------'
+print df.mean()
+print '\n'
+print df.mean(1)
+print '\n'
+s = pd.Series([1,3,5,np.nan,6,8], index=dates).shift(2)
+print s
+print '\n'
+print df.sub(s, axis='index')
