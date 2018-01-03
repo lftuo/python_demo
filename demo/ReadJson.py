1759c0a7f75e660fa4fc6a1e3146f91ac9bdbd20
tuotuo <909709223@qq.com>
2018-01-03 20:11:54 +0800

    上传最新代码

---
+++ demo/ReadJson.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+# @Author : tuolifeng
+# @Time : 2018/1/3 下午8:06
+# @File : ReadJson.py
+# @Software : IntelliJ IDEA
+# @Email ： 909709223@qq.com
+# 读取JSON文件
+import json
+
+with open('city.json', 'r') as json_file:
+    data = json.load(json_file)
+
+
+for x in data:
+    print x['city'],x['code']
