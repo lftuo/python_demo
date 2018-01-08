cc0e12a22fc943ea649077baeff389bca48dff6d
lftuo <909709223@qq.com>
2018-01-08 13:33:39 +0800

    上传最新代码

--- demo/PythonAnalysis.py
+++ demo/PythonAnalysis.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 conn= MySQLdb.connect(host='localhost',port = 3306,user='root',passwd='123456',db ='spider_lianjia',charset='utf8')
 cur = conn.cursor()
-sql = "SELECT city,area,avg(price) as avg_price FROM spider_lianjia.quanguo_xiaoqu_position_info where city = '杭州' and price != '暂无' group by area"
+sql = "SELECT city,area,avg(price) as avg_price FROM spider_lianjia.quanguo_xiaoqu_position_info where city = '南京' and price != '暂无' group by area"
 df = pd.read_sql(sql,conn,index_col=('area'))
 df.plot()
 plt.show()
